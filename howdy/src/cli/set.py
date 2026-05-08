b34f7f8d5d3a579d910d04c59dffa528f1e12ae4
nathawat <nathawat@noreply.codeberg.org>
2026-05-08 14:47:47 +0700

    security(cli): fix fileinput race conditions in set

--- howdy/src/cli/set.py
+++ howdy/src/cli/set.py
@@ -4,7 +4,7 @@
 import sys
 import os
 import builtins
-import fileinput
+import tempfile
 import paths_factory
 
 from i18n import _
@@ -25,20 +25,33 @@ set_value = builtins.howdy_args.arguments[1]
 
 # Will be filled with the correctly config line to update
 found_line = ""
+found_line_index = -1
+lines = []
 
 # Loop through all lines in the config file
-for line in fileinput.input([config_path]):
-	# Save the line if it starts with the requested config option
-	if line.startswith(set_name + " "):
-		found_line = line
+with open(config_path, "r") as f:
+	for i, line in enumerate(f):
+		lines.append(line)
+		if line.startswith(set_name + " "):
+			found_line = line
+			found_line_index = i
 
 # If we don't have the line it is not in the config file
 if not found_line:
 	print(_('Could not find a "{}" config option to set').format(set_name))
 	sys.exit(1)
 
-# Go through the file again and update the correct line
-for line in fileinput.input([config_path], inplace=1):
-	print(line.replace(found_line, set_name + " = " + set_value + "\n"), end="")
+lines[found_line_index] = set_name + " = " + set_value + "\n"
+
+# Write to temp file then atomically replace
+fd, temp_path = tempfile.mkstemp(dir=os.path.dirname(config_path), suffix=".tmp")
+try:
+	with os.fdopen(fd, "w") as tmp:
+		tmp.writelines(lines)
+	os.replace(temp_path, config_path)
+except Exception:
+	if os.path.exists(temp_path):
+		os.unlink(temp_path)
+	raise
 
 print(_("Config option updated"))
